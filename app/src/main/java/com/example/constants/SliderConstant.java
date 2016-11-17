4f29d1eb56b8ad34eeef83a520232315334a9a6f
DESKTOP-H92F3GH\mete_ <metesoydas@gmail.com>
2016-11-18 01:34:20 +0300

    Slider converted to Library module and added an example project.

---
+++ app/src/main/java/com/example/constants/SliderConstant.java
@@ -0,0 +1,44 @@
+package com.example.constants;
+
+import com.example.R;
+
+/**
+ * Created by mete_ on 18.11.2016.
+ */
+
+public class SliderConstant {
+
+    public static final String[] CATEGORY_LABEL_TEXTS =  new String[] {
+            "Animals",
+            "Clothes",
+            "Earth",
+            "Foods",
+            "FRUITS & VEGETABLES",
+            "Countries",
+            "Body",
+            "Goods"
+    };
+
+    public static int[] CATEGORY_IMAGE_RESOURCE_IDS = {
+            R.drawable.ic_category_1,
+            R.drawable.ic_category_5,
+            R.drawable.ic_category_4,
+            R.drawable.ic_category_3,
+            R.drawable.ic_category_2,
+            R.drawable.ic_category_6,
+            R.drawable.ic_category_7,
+            R.drawable.ic_category_8
+    };
+
+    public static int[] CATEGORY_LABEL_TEXT_COLORS = {
+            R.color.material_amber_500,
+            R.color.material_red_500,
+            R.color.material_green_500,
+            R.color.material_blue_500,
+            R.color.material_grey_500,
+            R.color.material_cyan_500,
+            R.color.material_indigo_500,
+            R.color.material_lime_500
+    };
+
+}
